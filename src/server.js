1901672073c7ae302b4369aa12736a907d82ac94
Glaiza Wagner <glaizawagner@gmail.com>
2019-11-04 21:43:20 -0700

    files updated

--- src/server.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-const app = require('./app');
-const { PORT } = require('./config');
-
-// const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
